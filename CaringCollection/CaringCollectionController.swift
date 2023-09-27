98aeb1e96381b331754a887a84495ad94d55e13c
Александр Горелкин <alexander@Aleksandrs-MacBook-Pro.local>
2023-09-27 19:03:17 +0300

    Remark

--- CaringCollection/CaringCollectionController.swift
+++ CaringCollection/CaringCollectionController.swift
@@ -1,5 +1,5 @@
 //
-//  CarringCollectionController.swift
+//  CaringCollectionController.swift
 //  CaringCollection
 //
 //  Created by Александр Горелкин on 25.09.2023.
@@ -7,73 +7,89 @@
 
 import UIKit
 
+
 final class CaringCollectionController: UIViewController {
     
-    static let itemWidth: CGFloat = UIScreen.main.bounds.width * 0.9
-    static let itemHeight: CGFloat = UIScreen.main.bounds.height * 0.7
+    private static let itemSize: CGSize = .init(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.7)
+    private static let itemSpacing: CGFloat = 16
     
-    var itemSize = CGSize(width: itemWidth, height: itemHeight)
+    // MARK: - Views
     
-    fileprivate let contentInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-    
-    private lazy var collectionFlowLayout: UICollectionViewFlowLayout = {
+    private lazy var collectionViewLayout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = itemSize
-        layout.minimumLineSpacing = 16
+        
+        layout.itemSize = Self.itemSize
+        layout.minimumLineSpacing = Self.itemSpacing
         layout.scrollDirection = .horizontal
+        
         return layout
     }()
     
-           
-           
-           
-    
-    private lazy var carringCollectionView: UICollectionView = {
-        let collection = UICollectionView(frame: .zero, collectionViewLayout: collectionFlowLayout)
-        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
-        collection.dataSource = self
-        collection.delegate = self
-        collection.translatesAutoresizingMaskIntoConstraints = false
-        collection.contentInset = contentInset
-        return collection
+    private lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        
+        view.showsHorizontalScrollIndicator = false
+        
+        view.dataSource = self
+        view.delegate = self
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
+        
+        return view
     }()
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Collection"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .white
-        view.addSubview(carringCollectionView)
+        
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(collectionView)
         NSLayoutConstraint.activate([
-            carringCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            carringCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            carringCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            carringCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
+        
+        collectionViewLayout.sectionInset.left = collectionView.layoutMargins.left
     }
-    
+     
 }
 extension CaringCollectionController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        20
+        return 12
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = carringCollectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
-        cell.contentView.backgroundColor = .systemGray
-        cell.contentView.layer.cornerRadius = 20
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
+        
+        cell.contentView.backgroundColor = .lightGray
+        cell.contentView.layer.cornerRadius = 12
+        
         return cell
     }
-    
-    
 }
+
 extension CaringCollectionController: UICollectionViewDelegate {
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let visibleRect = CGRect(origin: scrollView.contentOffset, size: scrollView.bounds.size)
-        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
-        guard let visibleIndexPath = carringCollectionView.indexPathForItem(at: visiblePoint) else { return }
-        carringCollectionView.scrollToItem(at: visibleIndexPath, at: .left, animated: true)
+    
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        let itemWidth = Self.itemSize.width + Self.itemSpacing
+        let contentOffset = targetContentOffset.pointee.x + Self.itemSpacing
+        let itemToScroll = round(contentOffset / itemWidth)
+        
+        targetContentOffset.pointee = CGPoint(x: itemToScroll * itemWidth - scrollView.contentInset.left,
+                                              y: scrollView.contentInset.top)
     }
     
 }
+
+
